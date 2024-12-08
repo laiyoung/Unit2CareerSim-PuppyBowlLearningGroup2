ab0737b2401a6f89454939b572b7f9fbe082fe5d
laiyoung <l.d.young@outlook.com>
2024-12-08 14:15:56 -0500

    all+singletest

--- script.test.js
+++ script.test.js
@@ -27,20 +27,37 @@ describe("fetchAllPlayers", () => {
   });
 });
 
+
 // TODO: Tests for `fetchSinglePlayer`
 describe("fetchSinglePlayer", () => {
-test("returns an array", async () => {
-  expect(Array.isArray(players)).toBe(true);
-});
-
-test("returns players with name and id", async () => {
-  players.forEach((player) => {
-    expect(player).toHaveProperty("name");
-    expect(player).toHaveProperty("id");
+  let player = fetchSinglePlayer();
+  test("returns an object", async () => {
+    expect(Object.isObject(player)).toBe(true);
   });
+
+test("returns player with name, id, image, and details", async () => {
+  expect(player).toHaveProperty("name");
+  expect(player).toHaveProperty("id");
+  expect(player).toHaveProperty("breed");
+  expect(player).toHaveProperty("image");
+  expect(player).toHaveProperty("team");
 });
 });
 
 // TODO: Tests for `addNewPlayer`
 
+renderAllPlayers();
 // (Optional) TODO: Tests for `removePlayer`
+
+renderAllPlayers();
+/** Syncs state with the API and rerenders */
+async function renderAllPlayers() {
+  // console.log("Inside render");
+  await fetchAllPlayers();
+  renderSinglePlayer();
+  renderNewPlayerForm();
+}
+
+// === Script ===
+
+render();
